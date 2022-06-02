2d6215994ac293afc97f097f94f90041dc5b3cc0
Marijan Kelava <marijankelava2@gmail.com>
2022-06-02 21:30:40 +0200

    Cleanup a bit more

--- src/Entity/Triangle.php
+++ src/Entity/Triangle.php
@@ -2,9 +2,7 @@
 
 namespace App\Entity;
 
-use App\Repository\TriangleRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * @ORM\Entity(repositoryClass=TriangleRepository::class)
