efd6baf085c4190ce9864e60c0d5515f17a889e6
Marijan Kelava <marijankelava2@gmail.com>
2022-06-03 07:50:08 +0200

    Cleanup

--- temporary/oldTriangleController.php
+++
@@ -1,9 +0,0 @@
-/**
-     * @Route("/triangle/{a}/{b}/{c}", name="app_triangle", defaults={"a"=null, "b"=null, "c"=null}, methods={"GET"})
-     */
-    public function index(): Response
-    {
-        return $this->json([
-            'controller_name' => 'TriangleController',
-        ]);
-    }
